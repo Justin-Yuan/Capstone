d6768dfc965d3187274e259e474229a4313f5631
Justin-Yuan <justin.zcyuan@gmail.com>
2020-01-19 14:53:00 -0500

    updated contest1 template code

--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -3,22 +3,60 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
 
 #include <stdio.h>
 #include <cmath>
 
 #include <chrono>
 
+#define N_BUMPER (3)
+#define RAD2DEG(rad) ((rad) * 180. / M_PI)
+#define DEG2RAD(deg) ((deg) * M_PI / 180.)
+
+// global vars 
+float angular = 0.0;
+float linear = 0.0;
+float posX = 0.0, posY = 0.0, yaw = 0.0; ​
+uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
+
+float minLaserDist = std::numeric_limits<float>::infinity();
+int32_t nLasers=0, desiredNLasers=0, desiredAngle=5;
+
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
-	//fill with your code
+	// Access using bumper[kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
+    bumper[msg->bumper] = msg->state;
 }
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-	//fill with your code
+	nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
+    desiredNLasers = DEG2RAD(desiredAngle)/msg->angle_increment;
+    ROS_INFO("Size of laser scan array: %i and size of offset: %i", nLasers, desiredNLasers);
+
+    if (desiredAngle * M_PI / 180 < msg->angle_max && -desiredAngle * M_PI / 180 > msg->angle_min) {
+        for (uint32_t laser_idx = nLasers / 2 - desiredNLasers; laser_idx < nLasers / 2 + desiredNLasers; ++laser_idx){
+            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+        }
+    }
+    else {
+        for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx) {
+            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+        }
+    }
 }
+
+void odomCallback (const nav_msgs::Odometry::ConstPtr& msg)
+{
+    posX = msg->pose.pose.position.x;
+    posY = msg->pose.pose.position.y;
+    yaw = tf::getYaw(msg->pose.pose.orientation);
+    ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, RAD2DEG(yaw));
+}
+
 
 int main(int argc, char **argv)
 {
@@ -30,6 +68,8 @@ int main(int argc, char **argv)
 
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
+    ros::Subscriber odom = nh.subscribe("odom", 1, odomCallback);
+
     ros::Rate loop_rate(10);
 
     geometry_msgs::Twist vel;
@@ -39,12 +79,43 @@ int main(int argc, char **argv)
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
 
-    float angular = 0.0;
-    float linear = 0.0;
 
     while(ros::ok() && secondsElapsed <= 480) {
+        ROS_INFO("Postion: (%f, %f) Orientation: %f degrees Range: %f", posX, posY, yaw*180/pi, maxLaserRange);
         ros::spinOnce();
-        //fill with your code
+        
+        // Check if any of the bumpers were pressed.
+        bool any_bumper_pressed = false;
+        for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx) {
+            any_bumper_pressed |= (bumper[b_idx] == kobuki_msgs::BumperEvent::PRESSED);
+        }
+        //
+        // Control logic after bumpers are being pressed.
+        ROS_INFO("Postion: (%f, %f) Orientation: %f degrees Range: %f", posX, posY, RAD2DEG(yaw), minLaserDist);
+        if (posX < 0.5 && yaw < M_PI / 12 && !any_bumper_pressed) {
+            angular = 0.0;
+            linear = 0.2;
+        }
+        else if (yaw < M_PI / 2 && posX > 0.5 && !any_bumper_pressed) {
+            angular = M_PI / 6;
+            linear = 0.0;
+        }
+        else if (minLaserDist > 1. && !any_bumper_pressed) {
+            linear = 0.1;
+            if (yaw < 17 / 36 * M_PI || posX > 0.6) {
+                angular = M_PI / 12.;
+            }
+            else if (yaw < 19 / 36 * M_PI || posX < 0.4) {
+                angular = -M_PI / 12.;
+            }
+            else {
+                angular = 0;
+            }
+        }
+        else {
+            angular = 0.0;
+            linear = 0.0;
+        }
 
         vel.angular.z = angular;
         vel.linear.x = linear;
