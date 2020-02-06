507d33ee68450b482aea35067b624ea96d62271f
Gary Leung <ytgary.leung@mail.utoronto.ca>
2020-02-06 11:59:31 -0500

    Added all of reference.cpp into planner.cpp

--- mie443_contest1/src/planners.cpp
+++ mie443_contest1/src/planners.cpp
@@ -1,15 +1,146 @@
 // #include "mie443_contest1/include/planners.h"
 #include "planners.h"
 
+/* Public Functions */
+
+void motionPlanner::startup()
+{
+    ROS_INFO("Performing Startup");
+    chooseDirection();
+}
+
 void motionPlanner::step()
 {
+    ROS_INFO("Stepping");
     ros::spinOnce();
-    checkBumpers();
-    threeRegion();
+    referenceMain();
 }
 
 
 /* Private Functions */
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Planning functions //////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void motionPlanner::referenceMain()
+{
+    float angular, linear;
+    // Offset Calculation
+    int left_ind = laserSize - 1 - laserOffset, right_ind = laserOffset;
+    int left_ind_offset = left_ind - ((laserSize - 1) / 2);
+    int right_ind_offset = ((laserSize - 1) / 2) - right_ind;
+
+    // Reevaluate the mode every certain durtaion
+    cout << time_passed << " " << time_last_update <<  " " << time_step << endl;
+    if (time_passed - time_last_update >= time_step) setMode();
+
+    if (mode == 2)
+    {
+        // In mode 2 Print information
+        ROS_INFO("LeftRange:%f,RightRange: %f", minLeftLaserDist, minRightLaserDist);
+    }
+    // Correction counter
+    if (sqrt((posX - x_turn) * (posX - x_turn) + (posY - y_turn) * (posY - y_turn)) > explore_per_dist && mode == 2)
+    {
+        x_turn = posX;
+        y_turn = posY;
+        chooseDirection();
+    }
+
+    ros::spinOnce();
+
+    // .....**E-STOP DO NOT TOUCH**.......
+    eStop.block();
+
+    // Bumper check
+    checkBumpers();
+
+    // Free Space movement
+    // Difference between mode 1 and 2
+    if (minLaserDist > 0.7)
+    {
+        if (mode == 1) // Define mode 1
+        {
+            // Define mode 1 speed
+            angular = 0.0;
+            linear = linear_max;
+            // Correction when it is too close to walls
+            if (minLeftLaserDist < 0.5)
+                angular = -angular_max;
+            else if (minRightLaserDist < 0.5)
+                angular = angular_max;
+        }
+        else if (mode == 2) // Define mode 2
+        {
+            // Define linear speed
+            angular = 0.0;
+            linear = linear_max;
+            // Overwrite the angular speed when the distances from two sides are different more than 0.2
+            if (minLeftLaserDist - minRightLaserDist > 0.2)
+                angular = (minLeftLaserDist - minRightLaserDist) /
+                          minLeftLaserDist * (0.5 * angular_max);
+            else if (minRightLaserDist - minLeftLaserDist > 0.2)
+                angular = (minRightLaserDist - minLeftLaserDist) /
+                          minRightLaserDist * (-0.5 * angular_max);
+            // Determines where distance values on both sides are located, and if a large discrepancy, turns
+            if (left_ind_offset + 100 < right_ind_offset)
+                angular = angular_max;
+            else if (right_ind_offset + 100 < left_ind_offset)
+                angular = -angular_max;
+            // Overwrite the angular speed when it is too close to wall
+            if (minLeftLaserDist < 0.5)
+                angular = -angular_max;
+            else if (minRightLaserDist < 0.5)
+                angular = angular_max;
+        }
+    }
+    // When the front sensor reading is too low
+    else if (minLaserDist < 0.5)
+    {
+        // Determine which side has more space
+        if (minRightLaserDist < minLeftLaserDist)
+        {
+            rotate2explore();
+        }
+        else
+        {
+            rotate2explore(CW);
+        }
+    }
+    else
+    {
+        // Tries to stabilize robot when close to hitting wall
+        if (minLeftLaserDist - minRightLaserDist > 0.2)
+            angular = (minLeftLaserDist - minRightLaserDist) /
+                      minLeftLaserDist * (0.75 * angular_max);
+        else if (minRightLaserDist - minLeftLaserDist > 0.2)
+            angular = (minRightLaserDist - minLeftLaserDist) /
+                      minRightLaserDist * (-0.75 * angular_max);
+        else
+            angular = 0.0;
+        // More gradual deceleration, can be extended for more gradual deceleration, more predictable than just
+        // decreasing by constant value until close to wall (previous method)
+        if (0.6 < minLaserDist <= 0.65)
+            linear = 0.65 * linear_max;
+        else if (0.55 < minLaserDist <= 0.6)
+            linear = 0.5 * linear_max;
+        else if (0.5 <= minLaserDist <= 0.55)
+            linear = 0.3 * linear_max;
+        else
+            linear = 0.8 * linear_max;
+    }
+
+    // Mode 0 for sensor test
+    if (mode == 0)
+    {
+        angular = 0;
+        linear = 0;
+    }
+
+    // write the defined speed to the robot
+    publishVelocity(angular, linear, true /* SpinOnce */);
+}
 
 void motionPlanner::checkBumpers()
 {
@@ -27,10 +158,10 @@ void motionPlanner::checkBumpers()
         }
 
         // Adjust angle away from obstacle
-        // if (bumper[kobuki_msgs::BumperEvent::RIGHT] == 1)
-        //     // rotate2angle(20);
-        // else if (bumper[kobuki_msgs::BumperEvent::LEFT] == 1)
-            // rotate2angle(20, CW);
+        if (bumper[kobuki_msgs::BumperEvent::RIGHT] == 1)
+            rotate2angle(20);
+        else if (bumper[kobuki_msgs::BumperEvent::LEFT] == 1)
+            rotate2angle(20, CW);
 
         // Maneuver forwards
         startX = posX; startY = posY;
@@ -40,10 +171,10 @@ void motionPlanner::checkBumpers()
         }
 
         // // Adjust angle back to original direction
-        // if (bumper[kobuki_msgs::BumperEvent::RIGHT] == 1)
-        //     // rotate2angle(20);
-        // else if (bumper[kobuki_msgs::BumperEvent::LEFT] == 1)
-            // rotate2angle(20, CW);
+        if (bumper[kobuki_msgs::BumperEvent::RIGHT] == 1)
+            rotate2angle(20);
+        else if (bumper[kobuki_msgs::BumperEvent::LEFT] == 1)
+            rotate2angle(20, CW);
     }
 }
 
@@ -74,6 +205,124 @@ geometry_msgs::Twist motionPlanner::threeRegion()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Rotation ////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool motionPlanner::inRange(int bin, const vector<double> & binRange, bool front /* = false */){
+    /**
+     * Check if the bin is in the desired zone
+     */
+    if (front){
+        return bin > binRange[0] || bin < binRange[1];
+    }
+    else {
+        return bin > binRange[0] && bin < binRange[1];
+    }
+}
+
+void motionPlanner::rotate2angle(float angle, bool CCW /* = true */) {
+    /**
+     * Rotate the robot to disred angle
+     * @param  {degree} float : degree in degrees
+     * @param  {CCW} bool : default rotation is CCW == turn left, set to false if need to turn right
+     */
+    // define velocity
+    double angular = angular_max;
+    if (!CCW)
+        angular = angular * -1;
+
+    // constraints
+    ros::spinOnce();
+    currYaw = yaw;
+    double rad = DEG2RAD(angle); // TODO: maybe move it to before passing to rotate
+
+    // rotate until desired
+    while (abs(yaw - currYaw) < rad)
+    {
+        publishVelocity(angular, 0.0, true /* SpinOnce */);
+    }
+}
+
+void motionPlanner::rotate2explore(bool CCW /* = true */) {
+    /**
+     * Rotate the robot until it is heading to a further wall/object
+     * @param  {CCW} bool : default rotation is CCW == turn left, set to false if need to turn right
+     */
+    double angular = angular_max;
+    if (!CCW)
+        angular = angular * -1;
+
+    // FIXME: change to the logic of when to stop
+    // Currently: stop turning (ready to go forward linearly) if there's something far away enough
+    while (minLaserDist < exploreDist || minLeftLaserDist < exploreDist_lr || minRightLaserDist < exploreDist_lr)
+    {
+        publishVelocity(angular, 0.0, true /* SpinOnce */);
+    }
+}
+
+void motionPlanner::rotate2bin(int bin)
+{
+    /**
+     * Command center of which rotate to perform
+     * @param  {int} bin : bin index
+     */
+    if (bin < exploreAngle_bins / 2)
+    {
+        rotate2angle(bin * exploreAngle_size);
+    }
+    else
+    {
+        rotate2angle((exploreAngle_bins - bin) * exploreAngle_size, CW);
+    }
+}
+
+// Correction function - the robot will rotate 360 degrees first. Then it will rotate to the direction that has the most space.
+// The robot should choose direction on its left or right side prior to the front side. Also, the robot will not turn back.
+void motionPlanner::chooseDirection() {
+    /**
+     * Rotate and choose direction to explore
+     */
+    // Define variables to store maximum value
+    int maxDist_front = 0, maxDist_front_idx = 0; // default is 0 zo that if things go weird, it just moves forward
+    int maxDist_side = 0, maxDist_side_idx = 0;
+
+    // Explore the front/left/right zones (no back zone!)
+    for (int bin = 0; bin < exploreAngle_bins; bin++)
+    {
+        ros::spinOnce();
+
+        // TODO: logic is fine, but might need to change the code appearance
+        if (inRange(bin, exploreZone_front, FRONT))
+        {
+            if (minLaserDist > maxDist_front)
+            {
+                maxDist_front = minLaserDist;
+                maxDist_front_idx = bin;
+            }
+        }
+        else if (inRange(bin, exploreZone_left) || inRange(bin, exploreZone_right))
+        {
+            if (minLaserDist > maxDist_side)
+            {
+                maxDist_side = minLaserDist;
+                maxDist_side_idx = bin;
+            }
+        }
+        rotate2angle(exploreAngle_size);
+    }
+
+    // Prefers to turn to the side in this mode
+    if (maxDist_side > exploreDist_side)
+    {
+        rotate2bin(maxDist_side_idx);
+    }
+    else
+    {
+        rotate2bin(maxDist_front_idx);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Helper functions ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -95,6 +344,34 @@ void motionPlanner::publishVelocity(float angular, float linear, bool spinOnce /
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Mode decision  //////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void motionPlanner::setMode() {
+    /**
+     * Choose a mode depending on the time passed, all variables are global
+     */
+    std::random_device device;
+    std::mt19937 gen(device());
+
+    ros::spinOnce();
+    time_passed =
+        std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - time_start).count();
+    random_prob = time_passed / time_total;
+ 
+    std::bernoulli_distribution randomOrNot(random_prob);
+    goRandom = randomOrNot(gen);
+    if (goRandom) {
+        mode = EXPLORE;
+    } else {
+        mode = FORWARD;
+    }
+    time_last_update = time_passed;
+
+    ROS_INFO("%f seconds, mode: %d, random_output: %d", (float) time_last_update, mode, goRandom);
+    return;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Callback functions //////////////////////////////////////////////////////////////////////////////////////
@@ -110,6 +387,7 @@ void motionPlanner::laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
     nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
     desiredNLasers = DEG2RAD(desiredAngle)/msg->angle_increment;
+    laserOffset = desiredAngle * M_PI / (180 * msg->angle_increment);
     laserScanTime = msg->scan_time;
     ROS_INFO("Size of laser scan array: %i, size of offset: %i, angle_max: %f, angle_min: %f, range_max: %f, range_min: %f, angle_increment: %f, scan_time: %f", 
         nLasers, desiredNLasers, msg->angle_max, msg->angle_min, msg->range_max, msg->range_min, msg->angle_increment, msg->scan_time);
