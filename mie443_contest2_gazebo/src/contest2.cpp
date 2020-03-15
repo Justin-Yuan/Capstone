19bb00bd881daa7d7802214eb4c63edc993658c0
Gary Leung <ytgary.leung@mail.utoronto.ca>
2020-03-15 17:06:50 -0400

    Changed gazebo folder files to our contest 2 files

--- mie443_contest2_gazebo/src/contest2.cpp
+++ mie443_contest2_gazebo/src/contest2.cpp
@@ -3,34 +3,40 @@
 #include <robot_pose.h>
 #include <imagePipeline.h>
 
-int main(int argc, char** argv) {
+// Matching status
+#define RAISIN 0
+#define CINNAMON 1
+#define RICE 2
+#define AMBIGUITY -1
+#define BLANK -2
+
+int main(int argc, char **argv)
+{
     // Setup ROS.
     ros::init(argc, argv, "contest2");
     ros::NodeHandle n;
-    // Robot pose object + subscriber.
-    RobotPose robotPose(0,0,0);
-    ros::Subscriber amclSub = n.subscribe("/amcl_pose", 1, &RobotPose::poseCallback, &robotPose);
+
     // Initialize box coordinates and templates
-    Boxes boxes; 
-    if(!boxes.load_coords() || !boxes.load_templates()) {
+    Boxes boxes;
+    if (!boxes.load_coords() || !boxes.load_templates())
+    {
         std::cout << "ERROR: could not load coords or templates" << std::endl;
         return -1;
     }
-    for(int i = 0; i < boxes.coords.size(); ++i) {
+    for (int i = 0; i < boxes.coords.size(); ++i)
+    {
         std::cout << "Box coordinates: " << std::endl;
-        std::cout << i << " x: " << boxes.coords[i][0] << " y: " << boxes.coords[i][1] << " z: " 
+        std::cout << i << " x: " << boxes.coords[i][0] << " y: " << boxes.coords[i][1] << " z: "
                   << boxes.coords[i][2] << std::endl;
     }
-    // Initialize image objectand subscriber.
-    ImagePipeline imagePipeline(n);
+
+    // nav contains subscriber to map_server, amcl and imagePipeline
+    Navigation nav(n, boxes, 3);
+
     // Execute strategy.
-    while(ros::ok()) {
-        ros::spinOnce();
-        /***YOUR CODE HERE***/
-        // Use: boxes.coords
-        // Use: robotPose.x, robotPose.y, robotPose.phi
-        imagePipeline.getTemplateID(boxes);
-        ros::Duration(0.01).sleep();
+    while (ros::ok())
+    {
+        nav.traverseAllBoxes();
     }
     return 0;
 }
